8da314a0dc76f0b5a54b8fccac33eff90ebf3737
Logan <logan@atomicorp.com>
2021-07-03 01:53:56 -0400

    Changes.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM golang:1.17-rc-buster
 WORKDIR /golang/src/rt-demo
+RUN go get -u github.com/lib/pq
 ENTRYPOINT ["/golang/src/rt-demo/build.sh"]
--- build/src/bin/main.go
+++ build/src/bin/main.go
@@ -1,31 +1,54 @@
 package main
 
 import (
-    "fmt"
-    "html"
+    //"fmt"
+    "html/template"
     "log"
     "net/http"
+    "io/ioutil"
+    "encoding/json"
+    _ "/root/go/pkg/mod/github.com/lib/pq"
 )
 
 func main() {
-
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	fmt.Fprintf(w, "Request sent: %#v",r)
-    })
-
-    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-        fmt.Fprintf(w, "Hi")
-    })
-
+    http.HandleFunc("/", EntryHandler)
+    http.HandleFunc("/fib_check", FibHandler)
+    http.Handle("/www/", http.StripPrefix("/www/", http.FileServer(http.Dir("www"))))
     log.Fatal(http.ListenAndServe(":6543", nil))
-
 }
 
+func EntryHandler(w http.ResponseWriter, r *http.Request) {
+    t, err := template.ParseFiles("./www/index.gohtml")
+    if err != nil {
+        w.Write([]byte("Error index.gohtml not found"))
+        return
+    }
+    tmp := struct{
+        Display string
+    }{
+        "",
+    }
+    w.Header().Set("Content-Type", "text/html")
+    t.ExecuteTemplate(w, "index", tmp)
+}
 
-
-/*
-static := http.FileServer(http.Dir("./static"))
-http.Handle("/",static)
-http.ListenAndServe(":8081",nil)
-*/
+func FibHandler(w http.ResponseWriter, r *http.Request) {
+    var req map[string]interface{}
+    w.Header().Set("Content-Type", "text/html")
+    body, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        w.Write([]byte(err.Error()))
+        return    
+    } 
+    err_unmarsh := json.Unmarshal(body, &req)
+    if err_unmarsh != nil {
+        w.Write([]byte(err_unmarsh.Error()))
+        return
+    } 
+    if _,ok := req["lookup"]; !ok {
+        w.Write([]byte("Error key not found in req map."))
+        return
+    }
+    //w.Write([]byte(req["lookup"].(string)))     
+    
+}
